9618f0c0d7faf9061a4017e7210142adad6da60f
zcrumpton <z.crumpton@gmail.com>
2020-08-08 10:22:57 -0700

    Added Art class

---
+++ MyFavoriteThings/FavoriteThings/Art.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFavoriteThings.FavoriteThings
+{
+    class Art
+    {
+
+        public string Style { get; set; }
+
+        public string Culture { get; set; }
+
+        public int Price { get; set; }
+
+        public bool isAvailable { get; set; }
+
+        public Art(string style, string culture, int price)
+        {
+            Style = style;
+            Culture = culture;
+            Price = price;
+
+            Console.WriteLine($"{style} got instantiated");
+            Console.WriteLine($"{culture} got instantiated");
+            Console.WriteLine($"{price} got instantiated");
+        }
+
+        public void Purchase(bool isAvailable)
+        {
+            if (isAvailable)
+            {
+                Console.WriteLine($"This {Style} art style is common amongst {Culture} cultures. It can be purchased for {Price} dollars!");
+            } else
+            {
+                Console.WriteLine($"This {Style} is common amongst {Culture} cultures. It has already been purchased for {Price} dollars.");
+            }
+        }
+
+        public void Purchase()
+        {
+            Purchase(true);
+        }
+
+    }
+}
